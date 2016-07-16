59f73a5597c3827238ab4aec9dacb635b32b7416
Helen Durrant <hh.durrant@gmail.com>
2016-07-16 15:14:34 +1200

    Added grouping by letter and responsiveness.

--- build/signs.js
+++ build/signs.js
@@ -11,6 +11,21 @@
             sign.id = sign.word.toLowerCase().match(/\w+/g).join('-');
         });
 
-        $('body').append(template(data));
+        data.sort(function (a, b) {
+            return a.word.toLowerCase().localeCompare(b.word.toLowerCase());
+        });
+
+        var groups = data.reduce(function (acc, sign) {
+            var letter = sign.word.substr(0, 1);
+            acc[letter] = acc[letter] || [];
+            acc[letter].push(sign);
+            return acc;
+        }, {});
+
+        $('body').append(template(groups));
+
+        $('.sgn-collapsable').on('hidden.bs.collapse', function (e) {
+            $(e.currentTarget).removeAttr('style');
+        });
     });
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,29 @@
 (function() {
     'use strict';
 
-    var template = Handlebars.compile($('#signs-template').html());
+    const template = Handlebars.compile($('#signs-template').html());
 
     $.getJSON('signs.json', function(data) {
 
-        data.forEach(function(sign) {
+        data.forEach(sign => {
             sign.id = sign.word.toLowerCase().match(/\w+/g).join('-');
         });
 
-        $('body').append(template(data));
+        data.sort((a, b) => {
+            return a.word.toLowerCase().localeCompare(b.word.toLowerCase());
+        });
+
+        const groups = data.reduce((acc, sign) => {
+            const letter = sign.word.substr(0, 1);
+            acc[letter] = acc[letter] || [];
+            acc[letter].push(sign);
+            return acc;
+        }, {});
+
+        $('body').append(template(groups));
+
+        $('.sgn-collapsable').on('hidden.bs.collapse', e => {
+            $(e.currentTarget).removeAttr('style');
+        });
     });
 }());
