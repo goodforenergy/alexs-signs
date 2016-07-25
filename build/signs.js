b0e1eba89be288da1eb12422f54f2cb279e6d30a
Philip Durrant (AP) <Philip.Durrant@dimensiondata.com>
2016-07-25 18:17:29 +1200

    Removed call to afterglow.js from main js file

--- build/signs.js
+++ build/signs.js
@@ -3,14 +3,6 @@
 (function () {
     'use strict';
 
-    (function initAfterglow() {
-        if (typeof afterglow !== 'undefined') {
-            afterglow.init();
-        } else {
-            setTimeout(initAfterglow, 50);
-        }
-    })();
-
     $('.sgn-collapsable').on('hidden.bs.collapse', function (e) {
         $(e.currentTarget).removeAttr('style');
     });
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,6 @@
 (function() {
     'use strict';
 
-    (function initAfterglow() {
-        if (typeof afterglow !== 'undefined') {
-            afterglow.init();
-        } else {
-            setTimeout(initAfterglow, 50);
-        }
-    }());
-
     $('.sgn-collapsable').on('hidden.bs.collapse', e => {
         $(e.currentTarget).removeAttr('style');
     });
